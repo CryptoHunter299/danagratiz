90b203159247f55d59a04ae00f27a365f9d62541
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2023-03-11 10:07:57 +0700

    Update danagartiz.php

--- danagartiz.php
+++ danagartiz.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Hayoo mau ngapain kak?
+Hayoo mau ngapain
 */
 
 echo "\n\e[0;31m]
@@ -14,26 +14,26 @@ echo "\n\e[0;31m]
 +=============UU====UU=======================+
 +              *//||\\*                      +
 +                ****                        +
-+ coded by : CryptoHunter299 | Pro Coder              +
++ coded by : CryptoHunter299 | Manusia              +
 + Team     : LNX#CREW   | S.T.C | BUFT         +
 + GitHub   : https://github.com/CryptoHunter299  +
 +============================================+
                               Version 1.1.1\e[0m \r\n";
 echo "\n\e[0;36m]
---==::{Saldo Dana Gratis}::==--";                              
+--==::{Thai Gratis}::==--";                              
 echo "\n\e[0;34m]Contoh : 0234xxxxxx";
 echo "\n\e[0;33m]
-Nomor Dana: \e[0m \r\n";
+Masukkan Thai ID Anda: \e[0m \r\n";
 $no = trim(fgets(STDIN, 1024));
 echo "\n\e[0;33m]
-Jumlah Dana Yang Anda Mau: \e[0m \r\n";
+Jumlah Thai Yang Anda Mau: \e[0m \r\n";
 $ju = trim(fgets(STDIN, 1024));
 echo "\n\e[0;32m]
-Otw Kirim Saldo Dana $ju Ke Nomor Dana $no\e[0m \r\n";
+$no , Ok, Otw Kirim Thai nya Sebesar , $ju\e[0m \r\n";
 echo "\n\e[0;34m]
-Harap Wait 24 Jam Ya Gan\e[0m \r\n";
+Harap Wait 200 Abad Ya Gan\e[0m \r\n";
 echo "\n\e[0;34m]
 Kalo Gak kekirim, anda kurang beruntung\e[0m \r\n";
 echo "\n\e[0;34m]
-Woakwokawokawok:v\e[0m \r\n";
+Btw Follow GitHub Gw Yak\e[0m \r\n";
 ?>
