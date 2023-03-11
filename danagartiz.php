e79f4ad7c61ba5caff4bc727593405981680feac
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2023-03-11 09:51:19 +0700

    Create danagartiz.php

---
+++ danagartiz.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Mau Ngapain Lu Liat-Liat Script Ini?
+*/
+
+echo "\n\e[0;31m]
+         .---.        .-----------
+        /     \  __  /    ------
+       / /     \(••)/    -----
+      //////   * \/ *   ---
+     //// / // :    : ---
+    // /   /  /*    *--
+   //          //..\\
++=============UU====UU=======================+
++              *//||\\*                      +
++                ****                        +
++ coded by : CryptoHunter299 | Pro Coder              +
++ Team     : LNX#CREW   | S.T.C | BUFT         +
++ GitHub   : https://github.com/CryptoHunter299  +
++============================================+
+                              Version 1.1.1\e[0m \r\n";
+echo "\n\e[0;36m]
+--==::{Formulir DANA Gratis}::==--";                              
+echo "\n\e[0;34m]Contoh : 081234567890";
+echo "\n\e[0;33m]
+Nomor DANA: \e[0m \r\n";
+$no = trim(fgets(STDIN, 1024));
+echo "\n\e[0;33m]
+Jumlah Yang Anda Mau: \e[0m \r\n";
+Rp$ju = trim(fgets(STDIN, 1024));
+echo "\n\e[0;32m]
+$no , Okey Tq Udh Ngisi Survey Ini , $ju\e[0m \r\n";
+echo "\n\e[0;34m]
+Lu Gabut y?:v\e[0m \r\n";
+echo "\n\e[0;34m]
+Eh, Lu Masih Gabut?:v\e[0m \r\n";
+echo "\n\e[0;34m]
+Btw Follow GitHub Gw Yak\e[0m \r\n";
+?>
